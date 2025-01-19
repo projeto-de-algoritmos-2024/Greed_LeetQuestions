de44b857fa44e303ff0e520feb9a721a4d0cb848
José_Viniciusq <viniciusa.s.queiroz@gmail.com>
2025-01-19 12:25:29 -0300

    Adicionando questoes

---
+++ 135.Candy/135_Candy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int criancas = ratings.size(); //obtendo o numero de crianças 
+        vector<int> doces(criancas, 1); // distribui um doce para cada crianca
+
+       
+        for (int i = 1; i < criancas; ++i) { //pecorre da primeira até a penultima 
+            if (ratings[i] > ratings[i - 1]) {
+                //se uma criança tiver uma classificaçao maior q a anterior, ela recebe mais um doce
+                doces[i] = doces[i - 1] + 1;
+            }
+        }
+
+       
+        for (int i = criancas - 2; i >= 0; --i) {// pecorre no sentido contrario
+            if (ratings[i] > ratings[i + 1]) {
+                q//Garante que se uma criança tem uma classificação maior que o vizinho da direita, ela terá mais doces que ele.
+                doces[i] = max(doces[i], doces[i + 1] + 1); 
+            }
+        }
+
+      
+        return accumulate(doces.begin(), doces.end(), 0);
+    }
+};
+
+    
---
+++ 2931.maximum-spending-after-buying-items/2931.maximum_spending_after_buying_tems.cpp
@@ -0,0 +1,30 @@
+class Solution{
+   public:
+    long long maxSpending(vector<vector<int>>& values) {
+        //pegando lojas 
+        int lojas = values.size();   
+        //itens por lojas     
+        int itens = values[0].size();    
+
+        //armazenado em um unico vetor todos os itens 
+        vector<int> combina;
+        for (int i = 0; i < lojas; i++) {
+            for (int j = 0; j < itens; j++) {
+                combina.push_back(values[i][j]);
+            }
+        }
+
+        // Ordenando em ordem cresultadocente 
+        sort(combina.begin(), combina.end());
+
+        //calculando 
+        long long resultado = 0;
+        int totalItens = itens * lojas;
+
+        for (int i = 1; i <= totalItens; i++) {
+            resultado += static_cast<ll>(i) * combina[i - 1];
+        }
+
+        return resultado;
+    }
+};
